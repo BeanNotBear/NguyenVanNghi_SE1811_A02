330ee3d98555e198c86723b2fa7396b3f7c9d18b
NghiNV <nghinv03@gmail.com>
2025-03-19 01:09:21 +0700

    Get all and create

---
+++ BLL/DTOs/CreateAccountDTO.cs
@@ -0,0 +1,27 @@
+﻿using Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BLL.DTOs
+{
+	public class CreateAccountDTO
+	{
+		[Required(ErrorMessage = "Account name is required.")]
+		[StringLength(100, ErrorMessage = "Account name must be between 3 and 100 characters.", MinimumLength = 3)]
+		public string AccountName { get; set; }
+
+		[Required(ErrorMessage = "Email is required.")]
+		[EmailAddress(ErrorMessage = "Invalid email format.")]
+		public string AccountEmail { get; set; }
+
+		[Required(ErrorMessage = "Role is required.")]
+		public AccountRole AccountRole { get; set; }
+
+		[Required(ErrorMessage = "Password is required.")]
+		[StringLength(100, ErrorMessage = "Password must be between 6 and 100 characters.", MinimumLength = 6)]
+		public string AccountPassword { get; set; }
+
+		[Required(ErrorMessage = "Confirm password is required.")]
+		[Compare("AccountPassword", ErrorMessage = "Passwords do not match.")]
+		public string ConfirmPassword { get; set; }
+	}
+}
---
+++ BLL/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using BLL.DTOs;
+using Shared.Enums;
+using System.Linq.Expressions;
+
+namespace BLL.Services.Interfaces
+{
+	public interface IAccountService
+	{
+		Task<IEnumerable<SystemAccountDTO>> GetAll(string? search = null, AccountRole? role = null);
+		Task Create(CreateAccountDTO createAccountDTO);
+	}
+}
